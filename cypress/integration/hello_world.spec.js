e8c39eea72bc99df1542dd6dee258982afe2e136
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2026-09-01 22:48:39 +0300

    update

--- cypress/integration/hello_world.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Hello World Test', () => {
-    it('should display hello world message', () => {
-        cy.visit('http://localhost:3000'); // Adjust the URL as needed
-        cy.contains('Hello World'); // Adjust the text as needed
-    });
-});
